7003de9546502cf5f7a422435967c451b483757f
Nhật Phi Võ <nhatphi1598753@gmail.com>
2023-03-03 14:46:05 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,50 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 function srcPath(subdir) {
   return path.join(__dirname, 'src', subdir);
 }
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "/build"),
-    filename: "bundle.js",
-    publicPath: "/",
-    assetModuleFilename: '[name][ext]',
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+    assetModuleFilename: '[name][ext]'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader']
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.s[ac]ss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource'
-      },
+      }
     ]
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     port: 3000,
+    allowedHosts: ['.preview.csb.app'],
     historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
+      template: './public/index.html'
     })
   ]
 };
